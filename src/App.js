a82df0591313eb4cd4f033abe0e903cbbdd00590
Udara Neminda Chandrawansa <udaraneminda@gmail.com>
2024-11-15 13:18:49 +0530

    nav done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import Nav from "./components/Nav";
+import banner_1 from "./images/banner/banner-1.png"
 
 function App() {
   return (
-    <div className="App">
-      <Nav></Nav>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="App h-full">
+      {/*Banner + Nav*/}
+      <div className="flex flex-col h-4/5" style={{backgroundImage: `url(${banner_1})`}}>
+        <div className="absolute w-full z-10 text-white">
+          <Nav></Nav>
+        </div>
+      </div>
+      
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,35 @@
-function Nav(){
-    return(
-        <div>
-            Nav
-        </div>
-    );
+function Nav() {
+  return (
+    <div className="group h-[50px]">
+      <div className="flex justify-between w-full px-10 h-[50px] opacity-0 group-hover:opacity-100 transition-all delay-100">
+        <ul className="flex w-1/3 justify-evenly">
+          <li className="flex items-center justify-center px-3 cursor-pointer">
+            Home
+          </li>
+          <li className="flex items-center justify-center px-3 cursor-pointer">
+            Company
+          </li>
+          <li className="flex items-center justify-center px-3 cursor-pointer">
+            Project
+          </li>
+          <li className="flex items-center justify-center px-3 cursor-pointer">
+            Partners
+          </li>
+        </ul>
+        <ul className="flex w-1/3 justify-evenly">
+          <li className="flex items-center justify-center px-3 cursor-pointer">
+            Careers
+          </li>
+          <li className="flex items-center justify-center px-3 cursor-pointer">
+            News & Events
+          </li>
+          <li className="flex items-center justify-center px-3 cursor-pointer">
+            Contact
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Nav;
