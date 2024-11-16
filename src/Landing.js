01d524e5de9ad9737e9ae8addba036074d874d3b
Udara Neminda Chandrawansa <udaraneminda@gmail.com>
2024-11-16 14:01:24 +0530

    carousel controls transparent

--- src/Landing.js
+++ src/Landing.js
@@ -208,6 +208,32 @@ function Landing() {
         {/* Carousel visible only on max-md */}
         <div className="hidden max-md:flex w-3/4">
           <Carousel
+            leftControl={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                className="bi bi-arrow-right-circle w-[30px] h-[30px] text-transparent"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"
+                />
+              </svg>
+            }
+            rightControl={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                className="bi bi-arrow-right-circle w-[30px] h-[30px] text-transparent"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"
+                />
+              </svg>
+            }
             indicators={false}
             className="rounded-se-3xl rounded-es-3xl bg-white transition-all delay-500"
           >
@@ -274,7 +300,35 @@ function Landing() {
         </p>
         {/* Carousel visible only on max-md */}
         <div className="hidden max-md:flex w-3/4 h-[100px]">
-          <Carousel indicators={false}>
+          <Carousel
+            leftControl={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                className="bi bi-arrow-right-circle w-[30px] h-[30px] text-transparent"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"
+                />
+              </svg>
+            }
+            rightControl={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                className="bi bi-arrow-right-circle w-[30px] h-[30px] text-transparent"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"
+                />
+              </svg>
+            }
+            indicators={false}
+          >
             <div className="flex gap-12 justify-center">
               <div
                 className="w-1/12 max-sm:w-1/6 aspect-square rounded-lg bg-no-repeat bg-contain"
